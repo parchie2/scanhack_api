20c0ae692b1c0eef905de574268ca8521cc14cdc
suzuki pri <pg.econ4869@gmail.com>
2020-11-01 23:16:26 +0900

    rubocop修正

--- app/services/google_cloud/translator_service.rb
+++ app/services/google_cloud/translator_service.rb
@@ -3,24 +3,23 @@
 require "google/cloud/translate"
 
 module GoogleCloud
-	class TranslatorService
-		# new(contents: ["こんにちわ"], lang: "en")
+  class TranslatorService
     def initialize(contents)
-			@contents = contents
+      @contents = contents
     end
 
     def call!
-			client = Google::Cloud::Translate.translation_service
+      client = Google::Cloud::Translate.translation_service
 
-			parent = client.location_path project: "scanhack", location: "global"
+      parent = client.location_path project: "scanhack", location: "global"
 
-			response = client.translate_text(parent: parent, contents: @contents, target_language_code: "en")
+      response = client.translate_text(parent: parent, contents: @contents, target_language_code: "en")
 
-			translations = []
-			response.translations.each do |translation|
-				translations.push(translation.translated_text)
-			end
-			translations
+      translations = []
+      response.translations.each do |translation|
+        translations.push(translation.translated_text)
+      end
+      translations
     end
   end
 end
