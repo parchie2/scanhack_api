2c4a9b10188f70c4a7224e499206761494016600
原島一桂 <ikkeiharashima@harashimahajimekatsuranoMacBook-Pro.local>
2020-11-01 23:15:13 +0900

    harashima did wonderful fix

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,10 +13,11 @@ module Api
 
       def create
         user = User.new(user_params)
+        logger.debug(user_params)
         if user.save
           render json: { status: 200, data: user }
         else
-          render json: { status: 400, message: user.errors.full_messages }
+          render status: :unprocessable_entity, json: { message: user.errors.full_messages }
         end
       end
 
