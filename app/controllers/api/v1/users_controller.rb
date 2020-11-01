b34e247d683504f7cbb7a1d344159015a01770c5
中條航紀 <ck227200@gmail.com>
2020-11-01 18:39:11 +0900

    :+1:Complete login

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
-module api
-  module v1
+module Api
+  module V1
     class UsersController < ApplicationController
-      # def index
-      #   companies = User.for_send
-      #   render json: { status: 'SUCCESS', data: companies }
-      # end
-      skip_before_action :authenticate_user, only: %i[create]
+      include ActionController::HttpAuthentication::Token::ControllerMethods
+      # before_action :authenticate
 
       def show
         user = User.find(params[:id])
@@ -25,22 +22,36 @@ module api
 
       def login
         user = User.find_by(name: params[:name])
-        if user&.authenticate(params[:password])
-          log_in(user)
-          render json: { status: 'SUCCESS', data: user }
+        if !user.nil?
+          render json: { status: 'SUCCESS', data: user.token }
         else
           render json: { status: 'ERROR', message: 'ログインに失敗しました' }
         end
       end
 
-      def logout
-        log_out
+      def current_user
+        current_user = current_send_user
+        render json: { status: 'SUCCESS', data: current_user }
       end
 
       private
 
       def user_params
         params.permit(:name)
+      end
+
+      def current_send_user
+        authenticate_or_request_with_http_token do |token, _options|
+          current_send_user = User.find_by(token: token)
+        end
+      end
+
+      def authenticate
+        authenticate_or_request_with_http_token do |token, _options|
+          auth_user = User.find_by(token: token)
+          !auth_user.nil? ? true : false
+          # !!auth_user
+        end
       end
     end
   end
