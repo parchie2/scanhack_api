4b15fb8a39cf490cafab942145ff26d94855b388
suzuki pri <pg.econ4869@gmail.com>
2020-11-01 18:47:03 +0900

    image annotation apiを作成

---
+++ app/services/google_cloud/image_annotator_service.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# Imports the Google Cloud client library
+require "google/cloud/vision"
+
+module GoogleCloud
+	class ImageAnnotatorService
+		def initialize(file_name)
+			@file_name = file_name
+		end
+
+		def call!
+			# Instantiates a client
+			image_annotator = Google::Cloud::Vision.image_annotator
+
+			# Performs label detection on the image file
+			labels = []
+			response = image_annotator.label_detection image: @file_name
+			response.responses.each do |res|
+				res.label_annotations.each do |label|
+					labels.push(label.description)
+				end
+			end
+			labels
+		end
+	end
+end
