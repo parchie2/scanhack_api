50fc3e38e790cb969fc0e9b701bdc52935859b25
中條航紀 <ck227200@gmail.com>
2020-11-02 11:24:11 +0900

    :+1:Add reference

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -16,8 +14,10 @@ ActiveRecord::Schema.define(version: 2020_11_01_122632) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,4 +27,6 @@ ActiveRecord::Schema.define(version: 2020_11_01_122632) do
     t.string "token"
     t.index ["name"], name: "index_users_on_name", unique: true
   end
+
+  add_foreign_key "items", "users"
 end
