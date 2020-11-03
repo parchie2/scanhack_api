7084566a0ae2acedbd9c4c91cf121cfd659cb2be
suzuki pri <pg.econ4869@gmail.com>
2020-11-04 00:07:29 +0900

    store file at local storage

--- app/controllers/api/v1/image_annotate_controller.rb
+++ app/controllers/api/v1/image_annotate_controller.rb
@@ -4,7 +4,10 @@ module Api
   module V1
     class ImageAnnotateController < ApplicationController
       def show
-        label_annotations = GoogleCloud::ImageAnnotatorService.new("./bag.jpg").call!
+        file_path = "tmp/#{current_user.id}"
+        File.open(file_path, "wb") { |f| f.write(image_data) }
+        label_annotations = GoogleCloud::ImageAnnotatorService.new(file_path).call!
+        FileUtils.rm(file_path)
         render status: :ok, json: { labels: label_annotations }
       rescue StandardError => error
         Rails.logger.error error
