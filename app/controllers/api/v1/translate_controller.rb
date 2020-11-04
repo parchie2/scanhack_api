527c0e21cf0743a6a5ad6b9fa81a2b6ce0c791dc
中條航紀 <ck227200@gmail.com>
2020-11-04 23:18:42 +0900

    :+1:Add current_user

--- app/controllers/api/v1/translate_controller.rb
+++ app/controllers/api/v1/translate_controller.rb
@@ -3,12 +3,28 @@
 module Api
   module V1
     class TranslateController < ApplicationController
+      include ActionController::HttpAuthentication::Token::ControllerMethods
       def show
-        translations = GoogleCloud::TranslatorService.new(["財布"]).call!
+        binding.pry
+        synonyms = RapidApi::SynonymProviderService.new(["bug", "wallet"]).call!
+        translations = GoogleCloud::TranslatorService.new(current_send_user.items.map(&:name)).call!
+        binding.pry
         render status: :ok, json: { translations: translations }
       rescue StandardError => error
         Rails.logger.error error
         render status: :unprocessable_entity
+      end
+      private
+      def current_send_user
+        authenticate_or_request_with_http_token do |token, _options|
+          User.find_by(token: token)
+        end
+      end
+
+      def authenticate
+        authenticate_or_request_with_http_token do |token, _options|
+          User.exists?(token: token)
+        end
       end
     end
   end
