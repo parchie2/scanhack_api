9f753508d057d2fbb49f90fa9e5b620c5751a4d4
ikkei12 <ikkei12.h@gmail.com>
2020-11-06 20:50:39 +0900

    fix

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,7 +11,7 @@ module Api
         render json: { data: user }
       end
 
-      def create
+      def create 
         user = User.new(user_params)
         if user.save
           render json: { data: user }
